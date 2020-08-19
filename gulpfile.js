a1e7183987c11a8a3d8253ea9e378632d7661be4
Galina Popova <g89117301783@gmail.com>
2020-08-19 19:05:44 +0300

    Работа с меню

--- gulpfile.js
+++ gulpfile.js
@@ -9,17 +9,16 @@ const sync = require("browser-sync").create();
 // Styles
 
 const styles = () => {
-  return gulp.src("source/less/style.less")
+  return gulp
+    .src("source/less/style.less")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(postcss([autoprefixer()]))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(sync.stream());
-}
+};
 
 exports.styles = styles;
 
@@ -28,14 +27,14 @@ exports.styles = styles;
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: "source",
     },
     cors: true,
     notify: false,
     ui: false,
   });
   done();
-}
+};
 
 exports.server = server;
 
@@ -44,8 +43,16 @@ exports.server = server;
 const watcher = () => {
   gulp.watch("source/less/**/*.less", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
-}
+};
 
-exports.default = gulp.series(
-  styles, server, watcher
-);
+exports.default = gulp.series(styles, server, watcher);
+
+// Image
+
+const imagemin = require("gulp-imagemin");
+
+exports.default = () => {
+  gulp
+    .src("sourse/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([imagemin.optipng({ optimizationLevel: 3 })]));
+};
